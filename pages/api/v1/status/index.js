36046dac0a4ca8727a144139646e8333eeaaf5be
GustavoOliani <oliani748@gmail.com>
2025-06-25 10:23:58 -0300

    add `databse.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,9 @@
-function status(request, response) {
-  response
-    .status(200)
-    .json({
-      chave: "O site ta no ar, mas ainda em construição"
-    });
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1+1 as sum;");
+  console.log(result.rows);
+  response.status(200).json({ chave: "O site ta no ar, mas ainda em construção" });
 }
 
 export default status;
